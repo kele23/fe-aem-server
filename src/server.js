8da169f8beba80eda50c1daad247b68a381b25e5
Michele Scala <m.scala@reply.it>
2024-01-18 17:28:32 +0100

    Test v4

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 //const HTLRender = require('./htl/htl-render');
-const StaticRepositoryReader = require('./resources/readers/static-repository-reader');
+//const StaticRepositoryReader = require('./resources/readers/static-repository-reader');
 const httpLoggerMiddleware = require('./middleware/http-logger-middleware');
-const rfMiddleware = require('./middleware/resource-founder-middleware');
-const AemRemoteRepositoryReader = require('./resources/readers/aem-remote-repository-reader');
+//const rfMiddleware = require('./middleware/resource-founder-middleware');
+//const AemRemoteRepositoryReader = require('./resources/readers/aem-remote-repository-reader');
 //const mtRender = require('./methods/render-get-method');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const Logger = require('./utils/logger');
-const rrMiddleware = require('./middleware/resource-resolver-middleware');
-const path = require('path');
+//const rrMiddleware = require('./middleware/resource-resolver-middleware');
+//const path = require('path');
 
 class Server {
     constructor(serverConfig) {
@@ -19,35 +19,35 @@ class Server {
             throw 'Missing content repos, please define at least one content repo';
         }
 
-        //make repository readers ( libs is taken from project source)
-        serverConfig.contentRepos.push({
-            rootPath: '/libs',
-            localPath: path.resolve(__dirname, 'repository'),
-            type: 'file',
-        });
+        // //make repository readers ( libs is taken from project source)
+        // serverConfig.contentRepos.push({
+        //     rootPath: '/libs',
+        //     localPath: path.resolve(__dirname, 'repository'),
+        //     type: 'file',
+        // });
 
         //make readers object
-        const crReposObj = {};
+        // const crReposObj = {};
 
-        //other paths can be loaded from configuration
-        for (let cr of serverConfig.contentRepos) {
-            switch (cr.type) {
-                case 'file':
-                case 'json':
-                    crReposObj[cr.rootPath] = new StaticRepositoryReader(cr.rootPath, cr.localPath, cr.options);
-                    break;
+        // //other paths can be loaded from configuration
+        // for (let cr of serverConfig.contentRepos) {
+        //     switch (cr.type) {
+        //         case 'file':
+        //         case 'json':
+        //             crReposObj[cr.rootPath] = new StaticRepositoryReader(cr.rootPath, cr.localPath, cr.options);
+        //             break;
 
-                case 'remote':
-                    crReposObj[cr.rootPath] = new AemRemoteRepositoryReader(cr.rootPath, cr.aemRemote, cr.options);
-                    break;
+        //         case 'remote':
+        //             crReposObj[cr.rootPath] = new AemRemoteRepositoryReader(cr.rootPath, cr.aemRemote, cr.options);
+        //             break;
 
-                case 'custom':
-                    crReposObj[cr.rootPath] = cr.reader;
-            }
-        }
+        //         case 'custom':
+        //             crReposObj[cr.rootPath] = cr.reader;
+        //     }
+        // }
 
         //create render and resource resolver
-        this.repoReadersObj = crReposObj;
+        // this.repoReadersObj = crReposObj;
         // this.render = new HTLRender(this.repoReadersObj, {
         //     modelAlias: this.serverConfig.modelAlias || ['model'],
         //     hotComponents: serverConfig.hotComponents,
@@ -55,11 +55,11 @@ class Server {
         this.proxies = serverConfig.proxies;
 
         // handle repo readers events
-        for (const value of Object.values(crReposObj)) {
-            value.on('repochanged', (data) => {
-                this.clients.forEach((client) => client.response.write(`data: ${JSON.stringify(data)}\n\n`));
-            });
-        }
+        // for (const value of Object.values(crReposObj)) {
+        //     value.on('repochanged', (data) => {
+        //         this.clients.forEach((client) => client.response.write(`data: ${JSON.stringify(data)}\n\n`));
+        //     });
+        // }
     }
 
     async buildExpress() {
@@ -82,17 +82,17 @@ class Server {
         }
 
         // hot reload
-        if (this.serverConfig.hotComponents) {
-            app.get('/repoevents', (req, resp) => {
-                this._handleRepoEvents(req, resp);
-            });
-            app.get('/htlHotClient.js', (req, res) => {
-                res.sendFile(path.resolve(__dirname, './static/client.js'));
-            });
-        }
+        // if (this.serverConfig.hotComponents) {
+        //     app.get('/repoevents', (req, resp) => {
+        //         this._handleRepoEvents(req, resp);
+        //     });
+        //     app.get('/htlHotClient.js', (req, res) => {
+        //         res.sendFile(path.resolve(__dirname, './static/client.js'));
+        //     });
+        // }
 
         // webpack / vite / dist files
-        await this._addCustomMiddlewares(app);
+        // await this._addCustomMiddlewares(app);
 
         // dist folder
         if (this.serverConfig.distFolder) {
@@ -112,8 +112,8 @@ class Server {
         }
 
         // resources ( limited to aem paths )
-        app.use('*', rrMiddleware(this.repoReadersObj));
-        app.use('*', rfMiddleware());
+        // app.use('*', rrMiddleware(this.repoReadersObj));
+        // app.use('*', rfMiddleware());
         // app.get('*', mtRender(this.render));
 
         return app;
@@ -121,25 +121,25 @@ class Server {
 
     _addCustomMiddlewares() {}
 
-    _handleRepoEvents(request, response) {
-        const headers = {
-            'Content-Type': 'text/event-stream',
-            Connection: 'keep-alive',
-            'Cache-Control': 'no-cache',
-        };
-        response.writeHead(200, headers);
+    // _handleRepoEvents(request, response) {
+    //     const headers = {
+    //         'Content-Type': 'text/event-stream',
+    //         Connection: 'keep-alive',
+    //         'Cache-Control': 'no-cache',
+    //     };
+    //     response.writeHead(200, headers);
 
-        const clientId = Date.now();
-        const newClient = {
-            id: clientId,
-            response,
-        };
+    //     const clientId = Date.now();
+    //     const newClient = {
+    //         id: clientId,
+    //         response,
+    //     };
 
-        this.clients.push(newClient);
-        request.on('close', () => {
-            this.clients = this.clients.filter((client) => client.id !== clientId);
-        });
-    }
+    //     this.clients.push(newClient);
+    //     request.on('close', () => {
+    //         this.clients = this.clients.filter((client) => client.id !== clientId);
+    //     });
+    // }
 }
 
 module.exports = Server;
